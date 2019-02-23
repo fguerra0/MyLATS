601ae1c93a1fd678efa79bba5d46d1009e80376e
Fernando Guerra <fguerra806@gmail.com>
2019-02-22 18:00:45 -0800

    Restructure of login and main pages

--- app/src/main/java/com/example/mylats/MainActivity.java
+++ app/src/main/java/com/example/mylats/MainActivity.java
@@ -19,7 +19,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+       // setContentView(R.layout.activity_login); // make new layout
     }
 
     public void basicReadWrite()
